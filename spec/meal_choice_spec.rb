ab324a0c41ae46a303de1a43004a035fed5ddd8a
Elliott Vargo <vargoelliott@gmail.com>
2020-11-10 01:46:16 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -6,7 +6,7 @@ describe 'meal_choice' do
   end
 
   it 'should allow you to set a protein' do
-    expect(meal_choice("broccoli","macoroni","tofu")).to eq("A plate of tofu with broccoli and macoroni.")
+    expect(meal_choice("broccoli", "macoroni", "tofu")).to eq("A plate of tofu with broccoli and macoroni.")
   end
   it 'should puts "What a nutritious meal!" and your order to the console' do
     expect($stdout).to receive(:puts).with("What a nutritious meal!")
